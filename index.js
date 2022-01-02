50a0b70162c05fcdcb519b4a0bd837ad17c68a2d
vatimetou ebeker <fatimaabcar2@gmail.com>
2022-01-02 13:05:08 -0500

    update

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+
+let event= "wonderful gifts"
+function writeCards( names ){
+
+    let arr = [ ];
+   
+
+    for ( let i=0; i < names.length; i++ ) {
+        const message = 'Thank you, '  +  names[ i ]+',' + ' for the wonderful surprise gift!' ;
+        arr.push( message );
+
+      }
+
+      return arr;
+    };
+
+    
+
+function countDown(n){
+    var k=[];
+
+   for (let i=0; i<n; i=i+n){
+       console.log(k[i]);
+   }
+}
+
